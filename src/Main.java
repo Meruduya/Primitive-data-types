e1f12e63785d6b591595c4de2956aaef81ebc8d4
Veronika <korynaitroyd@gmail.com>
2025-05-21 17:20:28 +0300

    Change in fail Main

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int price  = 13676; // стоимость билета
+        int price = 13676; // стоимость билета
         int miles = 20; // рублей за одну бонусную милю
 
-        int bonus = price / miles ; // рассчет бонусных миль
-        System.out.println("Начислено миль:"+ bonus);
-        }
+        int bonus = price / miles; // рассчет бонусных миль
+        System.out.println("Начислено миль:" + bonus);
+    }
 }
